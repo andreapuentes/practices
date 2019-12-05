91d5719c78e0fc89023c9a4639f700dea7237226
Andrea Puentes <andreampue@gmail.com>
2019-12-04 21:15:51 -0300

    commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -27,7 +27,7 @@ function App() {
           <div class="container">
             <h1 class="display-3">Hello, world!</h1>
             <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">About me</a></p>
           </div>
         </div>
 
@@ -55,8 +55,8 @@ function App() {
       </main>
 
       <footer class="container">
-        <p>&copy; Company 2017-2019</p>
-      </footer>
+        <p>&copy; Portfolio by Andrea Puentes</p>
+      </footer> 
     </>
   );
 }
